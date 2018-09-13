4fac66d2409c217822bf3ff8c57d833a002b92f3
Jeremy Ragsdale <jragsdale@jragsdale-ltm3.internal.salesforce.com>
2018-09-13 14:10:23 -0500

    Updated Readme & Heroku Button

--- config/dev.js
+++ config/dev.js
@@ -3,10 +3,10 @@ module.exports = {
     port: parseInt(process.env.DEV_APP_PORT) || 3000
   },
   Credentials: {
-    "ClientID": process.env.client_id,     
-    "ClientSeceret": process.env.client_seceret,
-    "ClientID2": "344c019c-1fa0-4854-9a23-9cb6f3a7f33d",     
-    "ClientSeceret2": "9ae8a1e0-869b-486b-b71d-dd0c54d75aa"
+    "ClientID2": process.env.client_id,     
+    "ClientSeceret2": process.env.client_seceret,
+    "ClientID": "344c019c-1fa0-4854-9a23-9cb6f3a7f33d",     
+    "ClientSeceret": "9ae8a1e0-869b-486b-b71d-dd0c54d75aaa"
   }
 };
 
